625ce2f379bfc896ac31298857f2b277ee0fd036
sigitp-git <sigitp@amazon.com>
2021-02-09 16:16:22 -0600

    kubeconfig

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# eks
+# EKS Notes
---
+++ kubeconfig.sh
@@ -0,0 +1,2 @@
+aws eks --region us-east-1 update-kubeconfig --name eksworkshop
+Updated context arn:aws:eks:us-east-1:1234567890:cluster/eksworkshop in /Users/myfolder/.kube/config
