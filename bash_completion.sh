16b22bbdb9f78fba1b6a787aa40a142e67b5ed6d
sigitp-git <sigitp@amazon.com>
2021-02-09 16:48:42 -0600

    eksctl

---
+++ bash_completion.sh
@@ -0,0 +1,10 @@
+# Macbook Bash Completion
+
+eksctl completion bash >> ~/.bash_completion
+. /usr/local/etc/bash_completion
+. ~/.bash_completion
+
+
+kubectl completion bash >>  ~/.bash_completion
+. /usr/local/etc/bash_completion
+. ~/.bash_completion
