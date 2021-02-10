9f3250151bcf931c9aeb46e0757e25f3bd4d83c6
sigitp-git <sigitp@amazon.com>
2021-02-10 17:19:35 -0600

    kube-dashboard

---
+++ dashboard.sh
@@ -0,0 +1,34 @@
+
+https://github.com/kubernetes/dashboard
+
+$ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+namespace/kubernetes-dashboard created
+serviceaccount/kubernetes-dashboard created
+service/kubernetes-dashboard created
+secret/kubernetes-dashboard-certs created
+secret/kubernetes-dashboard-csrf created
+secret/kubernetes-dashboard-key-holder created
+configmap/kubernetes-dashboard-settings created
+role.rbac.authorization.k8s.io/kubernetes-dashboard created
+clusterrole.rbac.authorization.k8s.io/kubernetes-dashboard created
+rolebinding.rbac.authorization.k8s.io/kubernetes-dashboard created
+clusterrolebinding.rbac.authorization.k8s.io/kubernetes-dashboard created
+deployment.apps/kubernetes-dashboard created
+service/dashboard-metrics-scraper created
+deployment.apps/dashboard-metrics-scraper created
+
+kubectl proxy --port=8080 --address=0.0.0.0 --disable-filter=true &
+$ jobs
+[1]+  Running                 kubectl proxy --port=8080 --address=0.0.0.0 --disable-filter=true &
+
+http://localhost:8080/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
+
+aws eks get-token --cluster-name eksworkshop-eksctl | jq -r '.status.token'
+
+// Note the get-token available on new aws cli, to update on mac use this:
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+Unauthorized (401): You have been logged out because your token has expired.
+
+do another aws eks get-token
---
+++ role.sh
@@ -0,0 +1,5 @@
+STACK_NAME=$(eksctl get nodegroup --cluster eksworkshop-eksctl -o json | jq -r '.[].StackName')
+ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
+echo "export ROLE_NAME=${ROLE_NAME}" | tee -a ~/.bash_profile
+
+kubectl describe configmap -n kube-system aws-auth
